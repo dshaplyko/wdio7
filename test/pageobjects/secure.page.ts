eea212d1d400f9da08167fe71ef99107ede3f1a7
Dzmitry_Shaplyka <Dzmitry_Shaplyka@epam.com>
2021-05-21 12:05:09 +0300

    tsconfig added

--- test/pageobjects/secure.page.ts
+++ test/pageobjects/secure.page.ts
@@ -4,10 +4,15 @@ import Page from './page';
  * sub page containing specific selectors and methods for a specific page
  */
 class SecurePage extends Page {
-    /**
-     * define selectors using getter methods
-     */
-    get flashAlert () { return $('#flash') }
+	/**
+	 * define selectors using getter methods
+	 */
+	get flashAlert() { return $('#flash') }
+
+	async getText(): Promise<string> {
+		//await browser.pause(3000);
+		return (await (await this.flashAlert).getText());
+	}
 }
 
 export default new SecurePage();
--- test/specs/example.e2e.ts
+++ test/specs/example.e2e.ts
@@ -1,15 +1,12 @@
-import LoginPage from  '../pageobjects/login.page';
+import LoginPage from '../pageobjects/login.page';
 import SecurePage from '../pageobjects/secure.page';
+import { expect } from 'chai';
 
 describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open();
-
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        await expect(SecurePage.flashAlert).toBeExisting();
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
-    });
+	it('should login with valid credentials', async () => {
+		await LoginPage.open();
+		await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+		expect(await SecurePage.flashAlert).to.exist;
+		expect(await SecurePage.getText()).to.include('You logged into a secure area!');
+	});
 });
-
-
